0f262803c5c19a0097f6876c0189143427809e12
khrore <super.stas.0978@gmail.com>
2025-09-25 16:47:33 +0300

    1-8 completed

--- 1_concepts/1_8_thread_safety/src/main.rs
+++ 1_concepts/1_8_thread_safety/src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    cell::{Cell, RefCell},
+    rc::Rc,
+    sync::{Arc, Mutex, MutexGuard},
+};
+
+struct OnlySync<'a> {
+    a: MutexGuard<'a, i32>,
+}
+
+struct OnlySend {
+    a: Cell<i32>,
+}
+
+struct IsSyncNotSend {
+    a: Arc<i32>,
+}
+
+struct SyncAndSend {
+    a: Rc<i32>,
+}
+
 fn main() {
     println!("Implement me!");
 }
